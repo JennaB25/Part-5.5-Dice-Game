391d521195a788c91718b993acc0b051ffac165a
362700239 <jennarbloor@gmail.com>
2023-02-27 11:01:58 -0500

    Add project files.

---
+++ Part 5.5 Dice Game/Die.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5._5_Dice_Game
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }        
+        public int Roll { get { return _roll; } }
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+        public void DrawRoll()
+        {
+            if (_roll == 1)
+            {
+                Console.WriteLine("┌─────────┐");
+                Console.WriteLine("│         │");
+                Console.WriteLine("│    *    │");
+                Console.WriteLine("│         │");
+                Console.WriteLine("└─────────┘");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("┌─────────┐");
+                Console.WriteLine("│         │");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("│         │");
+                Console.WriteLine("└─────────┘");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine("┌─────────┐");
+                Console.WriteLine("│ *       │");
+                Console.WriteLine("│    *    │");
+                Console.WriteLine("│       * │");
+                Console.WriteLine("└─────────┘");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("┌─────────┐");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("│         │");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("└─────────┘");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("┌─────────┐");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("│    *    │");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("└─────────┘");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("┌─────────┐");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("│  *   *  │");
+                Console.WriteLine("└─────────┘");
+            }
+        }
+    }
+}
---
+++ Part 5.5 Dice Game/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Part_5._5_Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {          
+            double bankAccountNum = 100.00;
+            Console.WriteLine("Dice Betting Game");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("");
+            Console.WriteLine("How much do you bet?");
+            Console.WriteLine($"Bank Account: {bankAccountNum}");          
+            string bet1 = Console.ReadLine();
+            bet1 = bet1.Replace("$", "");
+            double _bet1 = Convert.ToDouble(bet1);
+
+        }
+    }
+}
